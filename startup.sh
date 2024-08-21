8e7442072451a8a405d089626b5e238ba158c5b8
Nicole Brewer <nbrewer6@asu.edu>
2024-08-21 11:45:24 -0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,18 +1,18 @@
 #! /usr/bin/bash
 
-install gh cli if it is not present
-if ! command -v gh >/dev/null 2>&1; then
-   curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
-   sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
-   echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-   sudo apt update && sudo apt install gh
-   gh auth login
-   git config --global user.name "Nicole Brewer"
-   git config --global user.email "nbrewer6@asu.edu"
-
-else
-   echo "GitHub CLI (gh) is already installed."
-fi
+# install gh cli if it is not present
+#if ! command -v gh >/dev/null 2>&1; then
+#   curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+#   sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
+#   echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+#   sudo apt update && sudo apt install gh
+#   gh auth login
+#   git config --global user.name "Nicole Brewer"
+#   git config --global user.email "nbrewer6@asu.edu"
+#
+#else
+#   echo "GitHub CLI (gh) is already installed."
+#fi
 
 sudo apt update
 
